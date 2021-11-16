ae0be84a33ecd07b9528a45e0c61c7e1d0e8638b
Tania Tasmin <taniat.kaau@email.com>
2021-11-16 16:45:31 -0500

    Change counter to red for negative values

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
   
   
   $("textArea").on('keyup', function () {
+    if (140-event.target.value.length<0){
+      $(this).parent().children('div').children('output').css( "color", "red");
+    }
     $(this).parent().children('div').children('output').text(140-event.target.value.length);
   });
   
-
+// $(this).parent().children('div').children('output') or $('.counter') 
   
 });
